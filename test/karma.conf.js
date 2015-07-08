c6539ae3397c2656950157bdec80c477eac0d76e
Thais Martins <thais@theauberginepanda.com>
2015-07-08 17:51:30 -0300

    Add translate module

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-translate/angular-translate.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
